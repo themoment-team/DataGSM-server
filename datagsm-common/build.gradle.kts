f885d7e6822a4ccba12591e02e46e218461a68ff
wwwcomcomcomcom <iieiiergn@gmail.com>
2026-05-13 00:43:19 +0900

    add(shared): KMP 공유 모듈 생성 및 KSP 출력 소스 연결

--- datagsm-common/build.gradle.kts
+++ datagsm-common/build.gradle.kts
@@ -45,6 +45,11 @@ dependencies {
     api(dependency.Dependencies.QUERY_DSL)
     ksp(dependency.Dependencies.QUERY_DSL_PROCESSOR)
 
+    // KMP Export
+    implementation(project(":datagsm-ksp-annotations"))
+    ksp(project(":datagsm-ksp-processor"))
+
+
     // Database
     api(dependency.Dependencies.MYSQL_CONNECTOR)
 
@@ -68,8 +73,8 @@ kotlin {
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
     }
+}
 
-    sourceSets.main {
-        kotlin.srcDirs("build/generated/ksp/main/kotlin")
-    }
+ksp {
+    arg("kmpOutputDir", "${layout.buildDirectory.get().asFile}/generated/kmp-export/main/kotlin")
 }
